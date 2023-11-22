2c74cb1770931c3003ef4fa32e10f1ce33ca9428
Nathan Anteneh <nathanantenehpersonal@gmail.com>
2023-11-22 16:30:42 +0300

    question 11 done

--- day 1.js
+++ day 1.js
@@ -23,3 +23,11 @@ let result6 = String6.includes("jargon");
 
 console.log(result6);
 
+// 11. Use console.log() and escape characters to print the following pattern.
+
+console.log("1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125");
+
+
+// exercise 3
+
+//
