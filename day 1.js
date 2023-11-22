b873eb03f9e4cf7baf664dcb914167630cda2667
Nathan Anteneh <nathanantenehpersonal@gmail.com>
2023-11-22 16:13:01 +0300

    question 18

---
+++ day 1.js
@@ -0,0 +1,5 @@
+let theString = 'You cannot end a sentence with because because because is a conjunction';
+
+let lastIndex = theString.lastIndexOf("because");
+
+console.log("The last index of the work 'because' is " + lastIndex);
