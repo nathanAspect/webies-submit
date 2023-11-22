fe9767ef8e1ccaaaeaf5d652d3b245acb274a849
Nathan Anteneh <nathanantenehpersonal@gmail.com>
2023-11-22 16:19:09 +0300

    25 done

--- day 1.js
+++ day 1.js
@@ -1,5 +1,13 @@
+// 18. Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
 let theString = 'You cannot end a sentence with because because because is a conjunction';
 
 let lastIndex = theString.lastIndexOf("because");
 
 console.log("The last index of the work 'because' is " + lastIndex);
+
+
+// 25. Use repeat() method to print 30 Days Of JavaScript 2 times
+
+let stringOut = "30 Days fo JavaScript";
+console.log(stringOut.repeat(2));
